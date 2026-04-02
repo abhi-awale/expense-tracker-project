1989ef01ae6992f605bd8c9e26a4e68b5c053179
Abhishek Awale <abhishekawale@Mac.bbrouter>
2026-04-02 19:14:30 +0530

    SCRUM-8 Removed forced sync

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./src/app');
 const {port} = require('./src/config/env.config');
 const db = require('./src/config/db.config');
 
-db.sync({force:true})
+db.sync()
     .then((result) => {
         app.listen(port, () => {
             console.log(`Server started at port ${port}`);
