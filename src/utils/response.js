26849e08f4eb4fcfe5eaa15cddfd47a26154594f
Abhishek Awale <abhishekawale@Mac.bbrouter>
2026-04-03 12:25:02 +0530

    SCRUM-19 unique email validation required

---
+++ src/utils/response.js
@@ -0,0 +1,32 @@
+const httpCode = require('../utils/statusCodes');
+
+function success(res, message, statusCode = httpCode.OK_200, data = null) {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data
+  });
+}
+
+function error(res, message, statusCode = httpCode.INTERNAL_ERROR_500, errors = null) {
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errors
+  });
+}
+
+function paginated(res, message, data, pagination, statusCode = httpCode.OK_200) {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data,
+    pagination
+  });
+}
+
+module.exports = {
+    success,
+    error,
+    paginated
+}
